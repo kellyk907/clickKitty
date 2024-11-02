6e2d2541dba78083c93f1424d7430124049002b2
kelly_k <pinkflamingo681@gmail.com>
2024-11-02 15:08:10 -0400

    push button styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 var button = document.querySelector(".show-cat");
-var cat = document.querySelector (".cat");
+var cat = document.querySelector(".cat");
 
-button.addEventListener("click",function (){
+// // When user clicks on the button, function is triggered to show cat image
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    // console.log("Yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+  } else {
+    // console.log("No");
     cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+  }
 });
+
+
+//cat.classList.add("show")- from button event//
